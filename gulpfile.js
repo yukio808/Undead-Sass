6713c68f55b24adc7ee4d5ceb347d61893fce0cd
yukio yamamoto <aj.yamamoto@me.com>
2015-02-11 10:13:14 -1000

    all requirements finnished and pushed ready for code review

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
 
-  return gulp.src('./sass/*.scss')
+  return gulp.src('./sass/**/*.scss')
     .pipe(sass( { errLogToConsole: true } ))
     .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('watch_sass', function() {
 
-  gulp.watch('./sass/*.scss', ['sass']);
+  gulp.watch('./sass/**/*.scss', ['sass']);
 
 });
 
